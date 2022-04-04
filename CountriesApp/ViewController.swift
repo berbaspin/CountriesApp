45a8699ca210f5021991a7a86ff60508ffd4e4f3
Dmitry <dmitry.babaev@yahoo.com>
2022-04-04 21:46:50 +0300

    Initial Commit

---
+++ CountriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesApp
+//
+//  Created by Дмитрий Бабаев on 04.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
