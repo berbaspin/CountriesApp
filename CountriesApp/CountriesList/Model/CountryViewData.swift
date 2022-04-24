7b9669d14e184f491632932494f64d72751765fd
Dmitry <dmitry.babaev@yahoo.com>
2022-04-24 22:44:56 +0300

    Equatable

--- CountriesApp/CountriesList/Model/CountryViewData.swift
+++ CountriesApp/CountriesList/Model/CountryViewData.swift
@@ -9,19 +9,16 @@ import Foundation
 
 struct CountryViewData {
 
-    var name: String
-    var capital: String
-    var population: String
-    var continent: String
-    var description: String
-    var shortDescription: String
-    var flag: String
-    var images: [String]
+    let name: String
+    let capital: String
+    let population: String
+    let continent: String
+    let description: String
+    let shortDescription: String
+    let flag: String
+    let images: [String]
 
 }
 
 extension CountryViewData: Equatable {
-    static func == (lhs: CountryViewData, rhs: CountryViewData) -> Bool {
-        lhs.name == rhs.name
-    }
 }
