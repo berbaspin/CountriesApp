e3474f17a1275a5311d53b0656091c9904544bb4
Dmitry <dmitry.babaev@yahoo.com>
2022-04-24 22:34:59 +0300

    naming

--- CountriesApp/AssemblyBuilder.swift
+++ CountriesApp/AssemblyBuilder.swift
@@ -9,29 +9,29 @@ import UIKit
 
 protocol AssemblyBuilderProtocol {
     func createCoutriesListModule(router: RouterProtocol) -> UIViewController
-    func createCountryDetailsModule(country: CountryViewData?, router: RouterProtocol) -> UIViewController
+    func createCountryDetailsModule(country: CountryViewData, router: RouterProtocol) -> UIViewController
 }
 
-class AssenlyBuilder: AssemblyBuilderProtocol {
+final class AssenlyBuilder: AssemblyBuilderProtocol {
     func createCoutriesListModule(router: RouterProtocol) -> UIViewController {
-        let viewVC = CountriesListViewController()
+        let countriesListViewController = CountriesListViewController()
         let networkService = NetworkService()
         let networkDataFetcher = NetworkDataFetcher(networkService: networkService)
         let coreDataManager = CoreDataManager()
         let presenter = CountriesListPresenter(
-            view: viewVC,
+            view: countriesListViewController,
             networkDataFetcher: networkDataFetcher,
             coreDataManager: coreDataManager,
             router: router
         )
-        viewVC.presenter = presenter
-        return viewVC
+        countriesListViewController.presenter = presenter
+        return countriesListViewController
     }
 
-    func createCountryDetailsModule(country: CountryViewData?, router: RouterProtocol) -> UIViewController {
-        let viewVC = CountryDetailsViewController()
-        let presenter = CountryDetailsPresenter(view: viewVC, router: router, country: country)
-        viewVC.presenter = presenter
-        return viewVC
+    func createCountryDetailsModule(country: CountryViewData, router: RouterProtocol) -> UIViewController {
+        let countryDetailsViewController = CountryDetailsViewController()
+        let presenter = CountryDetailsPresenter(view: countryDetailsViewController, router: router, country: country)
+        countryDetailsViewController.presenter = presenter
+        return countryDetailsViewController
     }
 }
